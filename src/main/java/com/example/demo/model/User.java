31153045506ae2caee7b523610d14e388b825471
bala-amypo <bala@amypo.in>
2025-12-18 08:07:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role = "USER";
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    // getters & setters
+}
