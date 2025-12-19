b951ed853ba6d27e3115d20ba911058b1eb2cacd
bala-amypo <bala@amypo.in>
2025-12-19 09:06:47 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,30 +1,6 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String fullName;
-
-    @Column(unique = true)
-    private String email;
-
-    private String password;
-
-    private String role = "USER";
-
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    public void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-
-    
-}
+public Long getId() { return id; }
+public String getEmail() { return email; }
+public String getPassword() { return password; }
+public String getRole() { return role; }
+public void setRole(String role) { this.role = role; }
+public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
