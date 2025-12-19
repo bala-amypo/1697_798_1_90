1667b3b7edf7b88aa6b4e1ec0dd4b6c5b198d2b3
bala-amypo <bala@amypo.in>
2025-12-19 10:12:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/DuplicateRuleService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.DuplicateRule;
+
+public interface DuplicateRuleService {
+
+    DuplicateRule createRule(DuplicateRule rule);
+
+    List<DuplicateRule> getAllRules();
+
+    DuplicateRule getRule(Long id);
+}
