bbd2bbc583051d4ef2726a66a9d8c2a803782b64
bala-amypo <bala@amypo.in>
2025-12-21 16:09:36 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketService.java
+++ src/main/java/com/example/demo/service/TicketService.java
@@ -32,18 +32,4 @@ public interface TicketService {
 
     void deleteTicket(Long id);
 }
-// import java.util.List;
-// import com.example.demo.model.Ticket;
 
-// public interface TicketService {
-
-//     Ticket createTicket(Long userId, Long categoryId, Ticket ticket);
-
-//     List<Ticket> getAllTickets();
-
-//     Ticket getTicketById(Long id);
-
-//     Ticket updateTicket(Long id, Ticket ticket);
-
-//     void deleteTicket(Long id);
-// }
