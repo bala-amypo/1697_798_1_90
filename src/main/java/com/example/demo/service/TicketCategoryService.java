01388a3aca5e0e418bb987f7cb223361f4721576
bala-amypo <bala@amypo.in>
2025-12-26 10:38:39 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketCategoryService.java
+++ src/main/java/com/example/demo/service/TicketCategoryService.java
@@ -1,11 +1,24 @@
+// package com.example.demo.service;
+
+// import com.example.demo.model.TicketCategory;
+
+// public interface TicketCategoryService {
+//     TicketCategory createCategory(TicketCategory category);
+//     TicketCategory getCategory(Long id);
+// }
+
+
 package com.example.demo.service;
 
 import com.example.demo.model.TicketCategory;
+import java.util.List;
 
 public interface TicketCategoryService {
+
     TicketCategory createCategory(TicketCategory category);
+
+    List<TicketCategory> getAllCategories();   // ✅ ADD THIS
+
     TicketCategory getCategory(Long id);
 }
-
-
 
