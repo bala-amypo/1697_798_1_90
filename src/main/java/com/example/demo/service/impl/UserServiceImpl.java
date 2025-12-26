5800e919d69a97560aeaf503cd3e4a92df6921c0
bala-amypo <bala@amypo.in>
2025-12-26 15:06:00 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,13 +1,55 @@
+// package com.example.demo.service.impl;
+
+// import com.example.demo.model.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
+// import java.util.List;
+
+// public class UserServiceImpl implements UserService {
+//     private final UserRepository userRepository;
+
+//     public UserServiceImpl(UserRepository userRepository) {
+//         this.userRepository = userRepository;
+//     }
+
+//     @Override
+//     public User registerUser(User user) {
+//         if (userRepository.existsByEmail(user.getEmail())) {
+//             throw new RuntimeException("Email already exists");
+//         }
+//         return userRepository.save(user);
+//     }
+
+//     @Override
+//     public User getUser(Long id) {
+//         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+//     }
+
+//     @Override
+//     public List<User> getAllUsers() {
+//         return userRepository.findAll();
+//     }
+// }
+
+
+
 package com.example.demo.service.impl;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    public UserServiceImpl() {}
+    
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
