ebd0d934582934eae36e69067beec0a5f23bfb13
bala-amypo <bala@amypo.in>
2025-12-26 10:35:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/TicketCategoryRepository.java
+++ src/main/java/com/example/demo/repository/TicketCategoryRepository.java
@@ -1,14 +1,26 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.model.TicketCategory;
+// import java.util.List;
+// import java.util.Optional;
+
+// public interface TicketCategoryRepository {
+//     boolean existsByCategoryName(String categoryName);
+//     TicketCategory save(TicketCategory category);
+//     Optional<TicketCategory> findById(Long id);
+//     List<TicketCategory> findAll();
+// }
+
 package com.example.demo.repository;
 
 import com.example.demo.model.TicketCategory;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketCategoryRepository {
+public interface TicketCategoryRepository
+        extends JpaRepository<TicketCategory, Long> {
+
     boolean existsByCategoryName(String categoryName);
-    TicketCategory save(TicketCategory category);
-    Optional<TicketCategory> findById(Long id);
-    List<TicketCategory> findAll();
 }
+
 
 
