6ce817f7bbc5876ed7ba68d561f98ad12f19e9ec
bala-amypo <bala@amypo.in>
2025-12-26 15:03:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,25 +1,69 @@
+// package com.example.demo.controller;
+
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
+// import java.util.List;
+
+// public class UserController {
+//     private final UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     public User registerUser(User user) {
+//         return userService.registerUser(user);
+//     }
+
+//     public User getUser(Long id) {
+//         return userService.getUser(id);
+//     }
+
+//     public List<User> getAllUsers() {
+//         return userService.getAllUsers();
+//     }
+// }
+
+
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    private final UserService userService;
+    
+    @Autowired
+    private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    @PostMapping
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        try {
+            User savedUser = userService.registerUser(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
-    public User registerUser(User user) {
-        return userService.registerUser(user);
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        try {
+            User user = userService.getUser(id);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    public User getUser(Long id) {
-        return userService.getUser(id);
-    }
-
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 }
