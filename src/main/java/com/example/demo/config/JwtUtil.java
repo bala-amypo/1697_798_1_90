ddeb5a61de635132d087899e2247f1205eab3e32
bala-amypo <bala@amypo.in>
2025-12-28 17:05:15 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtUtil.java
+++ src/main/java/com/example/demo/config/JwtUtil.java
@@ -12,6 +12,53 @@
 
 
 
+// package com.example.demo.config;
+
+// import io.jsonwebtoken.*;
+// import io.jsonwebtoken.security.Keys;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
+// import java.security.Key;
+// import java.util.Date;
+
+// @Component
+// public class JwtUtil {
+//     @Value("${jwt.secret}")
+//     private String SECRET;
+    
+//     @Value("${jwt.expiration}")
+//     private int EXPIRATION;
+    
+//     private Key getSigningKey() {
+//         return Keys.hmacShaKeyFor(SECRET.getBytes());
+//     }
+
+//     public String generateToken(String email) {
+//         return Jwts.builder()
+//                 .setSubject(email)
+//                 .setIssuedAt(new Date())
+//                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+//                 .signWith(getSigningKey())
+//                 .compact();
+//     }
+
+//     public boolean validateToken(String token) {
+//         try {
+//             Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+//             return true;
+//         } catch (Exception e) {
+//             return false;
+//         }
+//     }
+
+//     public String getEmailFromToken(String token) {
+//         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build()
+//                 .parseClaimsJws(token).getBody().getSubject();
+//     }
+// }
+
+
+
 package com.example.demo.config;
 
 import io.jsonwebtoken.*;
@@ -56,4 +103,3 @@ public class JwtUtil {
                 .parseClaimsJws(token).getBody().getSubject();
     }
 }
-
